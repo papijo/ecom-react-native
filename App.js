e91ea0267790a27b7c79a9149d9b8d7aa8cb6393
papijo <joe.ebh100@live.com>
2022-10-09 17:45:30 +0100

    Payment Screen Feature

--- App.js
+++ App.js
@@ -9,11 +9,12 @@ import SingleProductScreen from "./src/Screens/SingleProductScreen";
 import CartScreen from "./src/Screens/CartScreen";
 import ProfileScreen from "./src/Screens/ProfileScreen";
 import ShippingScreen from "./src/Screens/ShippingScreen";
+import PaymentScreen from "./src/Screens/PaymentScreen";
 
 export default function App() {
   return (
     <NativeBaseProvider>
-      <ShippingScreen />
+      <PaymentScreen />
     </NativeBaseProvider>
   );
 }
--- src/Screens/PaymentScreen.js
+++ src/Screens/PaymentScreen.js
@@ -1,11 +1,114 @@
 import React from "react";
-import { Text, View } from "react-native";
+import {
+  Text,
+  Box,
+  Center,
+  ScrollView,
+  VStack,
+  FormControl,
+  Input,
+  HStack,
+  Image,
+  Spacer,
+} from "native-base";
+import Colors from "./../color";
+import { FontAwesome, Ionicons } from "@expo/vector-icons";
+import Buttone from "../Components/Button";
+
+const paymentMethods = [
+  {
+    image: require("../../assets/paypal.png"),
+    alt: "paypal",
+    icon: "Ionicons",
+  },
+  {
+    image: require("../../assets/mc.png"),
+    alt: "Master Card",
+    icon: "FontAwesome",
+  },
+  {
+    image: require("../../assets/visa.png"),
+    alt: "visa",
+    icon: "FontAwesome",
+  },
+  {
+    image: require("../../assets/gpay.png"),
+    alt: "google pay",
+    icon: "FontAwesome",
+  },
+  {
+    image: require("../../assets/apple.png"),
+    alt: "google pay",
+    icon: "FontAwesome",
+  },
+  {
+    image: require("../../assets/discover.png"),
+    alt: "google pay",
+    icon: "FontAwesome",
+  },
+];
 
 const PaymentScreen = () => {
   return (
-    <View>
-      <Text>PaymentScreen</Text>
-    </View>
+    <Box flex={1} safeArea bg={Colors.steelGray} py={5}>
+      {/* Header */}
+      <Center pb={15}>
+        <HStack>
+          <Text color={Colors.white} fontSize={14} bold mr={1}>
+            SELECT PAYMENT METHOD
+          </Text>
+          <FontAwesome name="cc-visa" size={15} color={Colors.white} pt={3} />
+        </HStack>
+      </Center>
+      {/* Selection */}
+      <Box h="full" bg={Colors.platinum} px={5}>
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <VStack space={6} mt={5}>
+            {paymentMethods.map((pm, index) => (
+              <HStack
+                key={index}
+                alignItems="center"
+                bg={Colors.white}
+                px={3}
+                py={1}
+                justifyContent="space-between"
+                rounded={10}
+              >
+                <Box>
+                  <Image
+                    w={60}
+                    h={50}
+                    resizeMode="contain"
+                    source={pm.image}
+                    alt="Payment"
+                  />
+                  <Spacer />
+                </Box>
+                {pm.icon === "Ionicons" ? (
+                  <Ionicons
+                    name="checkmark-circle"
+                    size={30}
+                    color={Colors.steelGray}
+                  />
+                ) : (
+                  <FontAwesome
+                    name="circle-thin"
+                    size={30}
+                    color={Colors.steelGray}
+                  />
+                )}
+              </HStack>
+            ))}
+            <Buttone bg={Colors.steelGray} mt={5} color={Colors.white}>
+              CONTINUE
+            </Buttone>
+            <Text italic textAlign="center">
+              Payment Method is <Text bold> Paypal</Text> by default.
+            </Text>
+          </VStack>
+        </ScrollView>
+      </Box>
+    </Box>
   );
 };
 
